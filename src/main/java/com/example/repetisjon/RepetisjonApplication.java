1a609fe36b4060698587d685ce0222d76aaf441e
HamidHamrah <S362052@oslomet.no>
2022-03-24 12:11:10 +0100

    Initial commit

---
+++ src/main/java/com/example/repetisjon/RepetisjonApplication.java
@@ -0,0 +1,13 @@
+package com.example.repetisjon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepetisjonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepetisjonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/repetisjon/RepetisjonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repetisjon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepetisjonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
